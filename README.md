a731c9084bfc42bc0fae23e33024edaceb9275d9
Alex_rghd <alexisrangheard@outlook.fr>
2025-09-01 10:26:27 +0200

    update README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # jolimoi
+
+## set up
+
+## stacks
+
+- Front: ReactJs Typescript Tailwind
+- Back: NodeJs Express Typescript
+- Test: Jest Supertest
+
+## Front
+
+Created with Vite, using Tailwind for design.
+
+## Back
+
+NodeJs API with express, typescript.
+
+Folders:
+
+- controllers: handle incoming HTTP, extract data from the request, call the appropriate service logic, and send back the response.
+  
+- middlewares: functions sit between the request and the response. Used to process, validate, or modify requests before they reach the controller.
+- models: handle data access logic.
+- routes: map HTTP endpoints to their corresponding controller functions
+- services: contain the business logic of your application.
+- utils: contains generic, reusable helper functions.
+- app.ts: defines and configures the Express application instance
+- server.ts: main entry point
+
+### Tests
--- back/src/controllers/roman.controller.ts
+++ back/src/controllers/roman.controller.ts
@@ -2,9 +2,7 @@ import { Request, Response } from "express";
 import { toRomanService } from "../services/roman.service";
 
 interface ApiResponse {
-  // success: boolean;
   data: string;
-  // message?: string;
 }
 
 interface RomanRequest {
@@ -20,7 +18,6 @@ export const convertToRoman = async (
 
     if (typeof number !== "number") {
       return res.status(400).json({
-        // success: false,
         data: "Number field missing or not a number",
       });
     }
@@ -28,25 +25,16 @@ export const convertToRoman = async (
     const romanNumeral = toRomanService(number);
 
     return res.status(200).json({
-      // success: true,
-      // data: {
-      //   number: number,
-      //   roman: romanNumeral,
-      // },
       data: romanNumeral,
-
-      // message: `${number} converti en ${romanNumeral} avec succès`,
     });
   } catch (error) {
     if (error instanceof Error) {
       return res.status(400).json({
-        // success: false,
         data: error.message,
       });
     }
 
     return res.status(500).json({
-      // success: false,
       data: "Erreur interne du serveur",
     });
   }
